9832e2ee1fa318666d38f1491c711bbf932d4410
Enrique <eluisdev@gmail.com>
2025-01-18 18:02:20 -0400

    changes in layout.tsx

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout({
     children: React.ReactNode;
 }>) {
     const user = await getUser()
-    if (!user) {
+    if (user === null) {
         redirect("/auth/login")
     }
 
